ae1f1f81af2403a3bca6e911f1deeca7c39f14dc
langford <langfordquarshie21@gmail.com>
2025-06-07 17:02:35 +0000

    feat: show location results with user country first

--- src/services/location.ts
+++ src/services/location.ts
@@ -1,7 +1,7 @@
 import { MapSearchResult } from "@/lib/types";
 
 export async function getUserCountry(): Promise<string | null> {
-  const res = await fetch("https://ipapi.co/json/");
+  const res = await fetch("https://ipapi.co/json/", { cache: "force-cache" }); // force cache the location response
   if (!res.ok) throw new Error("Failed to fetch IP info");
   const data = await res.json();
   return data.country_name || null;
@@ -11,10 +11,23 @@ export async function searchLocation(
   query: string,
 ): Promise<MapSearchResult[]> {
   const userCountry = await getUserCountry();
-  console.log("userCountry", userCountry);
   const res = await fetch(
-    `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query + `, ${userCountry}`)}`,
+    `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}`,
   );
+
   if (!res.ok) throw new Error("Failed to fetch IP info");
-  return await res.json();
+
+  const results = await res.json();
+  let finalResults = results;
+
+  // sort the response to make those with the `userCountry` value come up
+  if (userCountry) {
+    finalResults = results.sort((a: MapSearchResult, b: MapSearchResult) => {
+      const aIncludes = a.display_name.includes(userCountry) ? 0 : 1;
+      const bIncludes = b.display_name.includes(userCountry) ? 0 : 1;
+      return aIncludes - bIncludes;
+    });
+  }
+
+  return finalResults;
 }
